057c7bd6d3075a85858566c83f55b9e6d51fd6cd
alex <alex.gibbs.k@gmail.com>
2017-02-15 12:40:23 +0200

    cmsg

--- src/main/SomeClass.java
+++ src/main/SomeClass.java
@@ -12,21 +12,21 @@ public class SomeClass {
 
     @Init
     public void sayHello(){
-        System.out.println("INIT: Started ");
+        System.out.println("INIT method ");
     }
 
     @Asynk
     public void sayHelloInNewThread(){
-        System.out.println("Asynk: started");
+        System.out.println("Asynk method");
     }
 
     @Asynk
     public void sayAsynkTest(){
-        System.out.println("Asynk: test started");
+        System.out.println("Asynk method 2");
     }
 
     @Asynk
     public void sayAsynkTest2(){
-        System.out.println("Asynk: test2 started");
+        System.out.println("Asynk method 3");
     }
 }
